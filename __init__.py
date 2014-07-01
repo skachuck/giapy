7dfc4c2c9f96bf2d953e3addbae184e16beb2e96
skachuck <skachuck@gmail.com>
2014-07-01 10:22:44 -0400

    Small fix for import

--- __init__.py
+++ __init__.py
@@ -1,8 +1,11 @@
-import giapy.data_tools.emergedata
-import giapy.earth_tools.
-import giapy.ice_tools.icehistory as giapy.ice_tools
+import giapy.giasim
+import giapy.earth_tools.earth_two_d
+import giapy.ice_tools.icehistory
 import giapy.plot_tools
 import giapy.map_tools
+
+from giapy.data_tools.meltwater import gen_eustatic
+import giapy.data_tools.emergedata
 
 # Configure paths to important directories
 DATAPATH = '/Data/emergence_data/'
