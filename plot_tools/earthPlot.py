2717199aed07a631407d04ef517a1d75256a7b98
skachuck <skachuck@gmail.com>
2015-05-28 13:02:19 -0400

    Plotting displacement, stress, and gravity through the mantle.

--- plot_tools/earthPlot.py
+++ plot_tools/earthPlot.py
@@ -16,3 +16,16 @@ def plotViscDecay(earth, ax=None, nmin=1, nmax=-1, nskip=1,
         ax.plot(nDecay[:,1]+vislim, earth.times, **kw)
 
     return plt.gca()
+
+def plotProfiles(zarray, yEyV, axs=None, **kw):
+    if axs is None: fig, axs = plt.subplots(2, 5, figsize=(15,6),
+                                            sharex=True)
+
+    titles = ['$U_E$', '$V_E$', '$P_E$', '$Q_E$', '$\phi1$', 
+              '$g1$' , '$U_V$', '$V_V$', '$P_V$', '$Q_V$'   ]
+
+    for ax, prof, title in zip(axs.flatten(), yEyV, titles):
+        ax.plot(zarray, prof, **kw)
+        ax.set_title(title)
+    
+    return plt.gca()
