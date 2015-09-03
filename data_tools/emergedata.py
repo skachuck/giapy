Updated model emergence calculations. Allows smoother profile (50 points as well as the data points) and always includes t=0 (present). Also, emergence calculation is corrected by measuring rsl, not the topography (which measures to the top of grounded ice).

data_tools/emergedata.py
@@ -11,16 +11,16 @@ from .abstractDataClasses import AbsGeoTimeSeries, AbsGeoTimeSeriesContainer
 from .. import timestamp
 
 
-def calcEmergence(sim, emergedata):
+def calcEmergence(sim, emergedata, smooth=True):
     #TODO This function shouldn't have to know what's inside any object, let
     # alone a complicated one, like sim. Consider calculating rsl first and
     # passing it and calculated times in (ts, rsl, emergedata).
     # To reference to present day
     #u0 = sim.inputs.harmTrans.spectogrd(sim['topo'][-1])
-    u0 = sim['topo'][-1]
+    u0 = sim['rsl'][-1]
 
     uAtLocs = []
-    for ut in sim['topo']:
+    for ut in sim['rsl']:
         ut = u0 - ut
         interpfunc = sim.inputs.grid.create_interper(ut.T)
         uAtLocs.append(interpfunc.ev(emergedata.lons, emergedata.lats))
@@ -29,8 +29,12 @@ def calcEmergence(sim, emergedata):
 
     data = {}
     for uAtLoc, loc in zip(uAtLocs, emergedata):
-        timeseries = np.array([np.sort(loc.ts), 
-                    np.interp(np.sort(loc.ts), 
+        if smooth:
+            ts = np.union1d(np.sort(loc.ts), np.linspace(0, loc.ts.max()))
+        else:
+            ts = np.sort(loc.ts)
+        timeseries = np.array([ts, 
+                    np.interp(ts, 
                                 sim.inputs.out_times[::-1], uAtLoc[::-1])]).T
         data[loc.recnbr] = EmergeDatum(timeseries, 
                                         lat=loc.lat, 

giasim.py
@@ -384,11 +384,14 @@ class GiaSimGlobal(object):
         gravObserver = GravObserver(out_times, ntrunc, ns)
         loadObserver = LoadObserver(remTimes, ice.shape)
         wloadObserver = WaterLoadObserver(remTimes, ice.shape)
+        geoObserver = GeoidObserver(out_times, ntrunc, ns)
+
         eslUplObserver = TotalUpliftObserver(remTimes, ntrunc, ns)
         eslGeoObserver = GeoidObserver(remTimes, ntrunc, ns)
         topoObserver = TopoObserver(remTimes, ice.shape)
         eslObserver = EslObserver(remTimes)
         velObserver = VelObserver(out_times, ntrunc, ns)
+        rslObserver = HeightObserver(remTimes, ice.shape, 'rsl')
 
         observerDict = GiaSimOutput(self)
         observerDict.addObserver('upl'   , uplObserver)
@@ -397,11 +400,13 @@ class GiaSimGlobal(object):
         observerDict.addObserver('load'  , loadObserver)
         observerDict.addObserver('wload' , wloadObserver)
         observerDict.addObserver('vel'   , velObserver)
+        observerDict.addObserver('geo'   , geoObserver)
 
         observerDict.addObserver('eslUpl', eslUplObserver)
         observerDict.addObserver('eslGeo', eslGeoObserver)
         observerDict.addObserver('topo'  , topoObserver)
         observerDict.addObserver('esl'   , eslObserver)
+        observerDict.addObserver('rsl'   , rslObserver)
 
 
         # Use progressbar to track calculation if desired.
@@ -508,14 +513,21 @@ class GiaSimGlobal(object):
                         dGel = dGel + dGelp
                         continue
 
+                for o in observerDict:
+                    # Topography and load for time tb are updated and saved.
+                    o.loadStageUpdate(tb, dLoad=dLoad, topo=Tb, esl=esl,
+                                        dwLoad=dhwI, rsl=Tb-iceb)
+
             else:
                 dLoad = (iceb-icea)*DENICE/DENSEA
                 Tb = None
 
-            for o in observerDict:
-                # Topography and load for time tb are updated and saved.
-                o.loadStageUpdate(tb, dLoad=dLoad, topo=Tb, esl=esl,
-                                    dwLoad=dhwI)
+                for o in observerDict:
+                    # Topography and load for time tb are updated and saved.
+                    o.loadStageUpdate(tb, dLoad=dLoad)
+
+
+            
 
             # Transform load change into spherical harmonics.
             loadChangeSpec = self.harmTrans.grdtospec(dLoad)/NREM
@@ -662,6 +674,27 @@ class MOIObserver(AbstractEarthGiaSimObserver):
 
 class AngularMomentumObserver(AbstractEarthGiaSimObserver):
     pass
+
+class HeightObserver(AbstractGiaSimObserver):
+    def __init__(self, outTimes, iceShape, name):
+        self.initialize(outTimes, iceShape)
+        self.name = name
+
+    def initialize(self, outTimes, iceShape):
+        self.array = np.zeros((len(outTimes), 
+                                iceShape[0], iceShape[1]))
+        self.outTimes = outTimes
+
+    def loadStageUpdate(self, tout, **kwargs):
+        if self.name in kwargs.keys():
+            self.update(tout, kwargs[self.name])
+
+    def update(self, tout, load):
+        if tout not in self.outTimes:
+            return
+        n = self.locateByTime(tout)
+        self.array[n] = load
+
 
 class LoadObserver(AbstractGiaSimObserver):
     def __init__(self, outTimes, iceShape):