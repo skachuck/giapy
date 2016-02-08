Allowed for arbitrary output times during decay calculation. Also, bug fixed with lame parameter in Earth params.

giapy/earth_tools/earthIntegrator.py
@@ -78,6 +78,7 @@ def integrateRelaxationScipy(f, out, atol=1e-6, rtol=1e-5):
     timeswrite = out.times
     dts = timeswrite[1:]-timeswrite[:-1]
 
+
     for dt in dts:
         r.integrate(r.t+dt)
         out.out(r.t, r.y[nz-1], r.y[2*nz-1], f)
@@ -180,13 +181,16 @@ class SphericalEarthOutput(object):
         is met.
         
     """
-    def __init__(self):
+    def __init__(self, times=None):
         secs_per_year = 3.1536e+7  # seconds in a year
-        self.times = secs_per_year*1e+3*\
-            np.array([0., 0.2, 0.5, 1., 2., 3., 4., 5., 6., 7., 8., 9., 
+        if times is not None:
+            self.times = times[:]
+        else:
+            self.times = np.array([0., 0.2, 0.5, 1., 2., 3., 4., 5., 6., 7., 8., 9., 
                       10., 12., 13., 14., 15., 16., 18., 21., 25., 30., 
-                      40., 50., 70., 90., 110., 130., 150.])
+                      40., 50., 70., 90., 110., 130., 150.])            
 
+        self.times = self.times*secs_per_year*1e+3
         #   Ue  Uv  Ve  Vv  phi1    g1  vels
         self.outArray = np.zeros((len(self.times), 7))
 

giapy/earth_tools/earthParams.py
@@ -84,7 +84,7 @@ class EarthParams(object):
                                                 # shear modulus.
         # Convert the bulk modulus to the first lame parameter.
         #TODO Do we want first lame parameter or bulk modulus?
-        prem[1:, 2] = prem[1:, 2]-2./3.*prem[1:, 3]
+        prem[1:, 2] = prem[1:, 2] - (2./3.*prem[1:, 3])
 
         # Create density gradient, g/cc.earthRadii
         dend = np.gradient(prem[1:,1])/np.gradient(z)

giapy/earth_tools/earthSpherical.py
@@ -64,7 +64,7 @@ class SphericalEarth(object):
     def setDesc(self, string):
         self._desc = string 
             
-    def calcResponse(self, zarray, nmax=100, nstart=None):
+    def calcResponse(self, zarray, nmax=100, nstart=None, times=None):
         """Calculate the response of the Earth to order numbers up to nmax.
         """
         
@@ -75,7 +75,7 @@ class SphericalEarth(object):
 
         respArray = []
         for n in range(nstart, nmax+1):
-            out = self.timeEvolve(n, zarray, nstart)
+            out = self.timeEvolve(n, zarray, nstart, times)
             respArray.append(out.outArray)
 
         respArray = np.array(respArray)
@@ -88,12 +88,12 @@ class SphericalEarth(object):
             self.respArray = np.r_[self.respArray, respArray]
 
 
-        self.times = out.times / 3.1536e10  # convert to thousand years
+        self.times = out.times / 3.1536e10  # convert back to thousand years
         self.nmax = nmax
         self.respInterp = interp1d(self.times, self.respArray, axis=1)
 
-    def timeEvolve(self, n, zarray, nstart=None):
-        out = SphericalEarthOutput()
+    def timeEvolve(self, n, zarray, nstart=None, times=None):
+        out = SphericalEarthOutput(times)
         
         if n == nstart or nstart is None:
             # For the first one, set up the relaxer and initial guess.