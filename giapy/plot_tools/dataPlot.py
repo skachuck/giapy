36cc73f8ece00fdfcbeec800f5c3c7c9598d1c3f
skachuck <skachuck@gmail.com>
2016-10-17 11:38:04 -0400

    Enhanced options for dataPlot.

--- giapy/plot_tools/dataPlot.py
+++ giapy/plot_tools/dataPlot.py
@@ -5,7 +5,7 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 from mpl_toolkits.basemap import Basemap
 
 def plotStdErrorsOnMap(lons, lats, ses, numPts=None, basemap=None, ax=None,
-                        ptids=None):
+                        ptids=None, clabel=None, vmax=None):
     basemap = basemap or Basemap()
     if ax is None:
         fig, ax = plt.subplots(1,1, figsize=(15,10))
@@ -16,22 +16,31 @@ def plotStdErrorsOnMap(lons, lats, ses, numPts=None, basemap=None, ax=None,
         normPts = numPts/float(max(numPts))
         s = 500*normPts
 
+    clabel = clabel or 'Standard Error (m)'
+
     datamax = np.mean(np.abs(ses)) + np.std(np.abs(ses))
     datamag = np.floor(np.log10(datamax))
-    vmax = (10**datamag)*np.maximum(
-            np.floor(datamax/(10**datamag))*2, 1.)
-    vmin=-vmax
+    if vmax is None:
+        vmax = (10**datamag)*np.maximum(
+                np.floor(datamax/(10**datamag))*2, 1.)
+
+    if np.alltrue(ses >= 0):
+        cmap = 'Reds'
+        vmin = 0
+    else:
+        cmap = 'RdYlBu_r'
+        vmin=-vmax
 
 
     basemap.drawcoastlines(color=(1,1,1,1), ax=ax, zorder=0)
     xs, ys = basemap(lons, lats)
     p = basemap.scatter(xs, ys, c=ses, s=s, 
-                        vmin=vmin, vmax=vmax, cmap='RdYlBu_r', 
+                        vmin=vmin, vmax=vmax, cmap=cmap, 
                         ax=ax, edgecolor='None', alpha=0.5)
     ax.set_axis_bgcolor((0,0,0,0.2))
     divider = make_axes_locatable(ax)
     cax = divider.append_axes('right', size='2%', pad=0.05)
-    plt.colorbar(p, cax=cax, label='Standard Error (m)')
+    plt.colorbar(p, cax=cax, label=clabel)
 
     if ptids is not None:
         for x, y, ptid in zip(xs, ys, ptids):
