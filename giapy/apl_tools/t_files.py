caddc6f8760e96f7b134a71117f1a75c8dd546e0
skachuck <skachuck@gmail.com>
2017-08-09 12:18:00 -0400

    Method to save out results to be read into APL program

--- giapy/apl_tools/t_files.py
+++ giapy/apl_tools/t_files.py
@@ -1,4 +1,5 @@
 import numpy as np
+import os
 
 def read_t_files(directory, filenames, data_col=2):
     """Read in a full comma-delimitted ice file in x-y-z format.
@@ -6,7 +7,7 @@ def read_t_files(directory, filenames, data_col=2):
     Parameters:
         filename - the file to be read
         Nx - number of latitude sites
-        Ny - number of longitude sites
+        Ny - number of longitude sitesb
 
     Returns:
         lat, lon - size (Nx, Ny) arrays of latitude and longitude measures
@@ -32,3 +33,48 @@ def read_t_files(directory, filenames, data_col=2):
     height = np.reshape(height, (-1, Nlat, Nlon))
         
     return Lat, Lon, height
+
+def write_case_files(casename, result): 
+
+    os.mkdir(casename)
+
+    coltit = 'longitude\tlatitude\tTotUpl\tTotUpl\tRateUpl\tGeoid\t'
+    coltit += 'emergence\twload\tload\tload\ticeload\tocean\ttopomap0'
+
+    result.upl.transform(result.inputs.harmTrans, inverse=False)
+    result.vel.transform(result.inputs.harmTrans, inverse=False)
+    result.geo.transform(result.inputs.harmTrans, inverse=False)
+
+    outTimes = result.upl.outTimes 
+    fnames = []
+
+    for i, t in enumerate(outTimes):
+        ai = np.vstack([result.inputs.grid.Lon.flatten(), 
+                   result.inputs.grid.Lat.flatten(), 
+                   result.upl[i].flatten(),
+                   result.upl[i].flatten(),
+                   result.vel[i].flatten(),
+                   result.geo[i].flatten(),
+                   (result.sstopo[i] - result.sstopo[-1]).flatten(),
+                   result.wload[i].flatten(),
+                   result.load[i].flatten(),
+                   result.load[i].flatten(),
+                   (result.load[i]- result.wload[i]).flatten(),
+                   (result.sstopo[i]<0).flatten(),
+                   result.inputs.topo.flatten()]).T
+
+        fname = '{}/py_file_{}.txt'.format(casename, i+1)
+        header = 'case: {} at {}\n'.format(casename, t) + coltit
+        np.savetxt(fname, ai, header=header)
+
+        fnames.append(fname)
+
+    with open('{}/py_file_inf.txt'.format(casename), 'w') as f:
+        f.write('case: {}\n'.format(casename))
+        f.write('date: {}\n'.format(result.TIMESTAMP))
+        f.write('vers: {}\n'.format(result.GITVERSION))
+        f.write('files: {}\n'.format('\t'.join(fnames)))
+        f.write('mMW: {}\n'.format('\t'.join([str(t) for t in result.esl.array])))
+        f.write('ages: {}\n'.format('\t'.join([str(t) for t in outTimes])))
+        
+
