596c3a30e667092d6335150fcda11ae95155762c
skachuck <skachuck@gmail.com>
2017-08-10 06:43:39 -0400

    init fix

--- giapy/apl_tools/__init__.py
+++ giapy/apl_tools/__init__.py
@@ -1,10 +1,1 @@
-import .data_tools
-import .earth_tools
-import .ice_tools
-import .plot_tools
 
-# Configure paths to important directories
-DATAPATH = '/Data/emergence_data/'
-EARTHPATH = ''
-ICEPATH = ''
-WRITEPATH = ''
